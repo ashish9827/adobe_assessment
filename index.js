705e50685aee4d1e6dcd6f2abe5f99b528f92988
ashish9827 <ashish335577@gmail.com>
2021-06-28 00:34:13 +0530

    uploaded

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
+//These three are npm packages
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const credential = require('./credential.json');
 const files = require('./lib/files');
 const inquirer  = require('./lib/inquirer');
 
+//This code is created by conversation history and I'm using my own credentials here
+
 const { WebClient, LogLevel } = require("@slack/web-api");
 // WebClient insantiates a client that can call API methods
-// When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-const client = new WebClient("xoxb-2197446421379-2196090874581-09TkotiNLhv2c17078tcVUx4", {
-  // LogLevel can be imported and used to make debugging simpler
-  //logLevel: LogLevel.DEBUG
-});
-var unirest = require('unirest');
+const client = new WebClient(credential.botUserOAuthToken, {});
+var unirest = require('unirest'); // It is used for API call
 
-
-clear(); //to clear the terminal before terminal starts
-
-console.log(
-  chalk.yellow( //chalk is used to color text in CLI
+//When program execute, It will clear the terminal first
+clear();
+//this function print Ashish in yellow color in terminal
+console.log( 
+  chalk.yellow(
     figlet.textSync('Ashish', { horizontalLayout: 'full' })
   )
 );
 
 const run = async () => {
     const credentials = await inquirer.askGithubCredentials();
-    if(credentials.username.includes(`"history"`)){
-      test();
+    // These conditions are my own implementation.
+    if(credentials.username.includes(`history`)  && credentials.username.includes(`"general"`)){
+      test(credential.channelIdGeneral);
+    }
+    else if(credentials.username.includes(`history`)  && credentials.username.includes(`"learning"`)){
+      test(credential.channelIdLearning);
     }
     else if(credentials.username.includes(`channel`) && credentials.username.includes(`message`)){
       groupMessage(credentials.username);
@@ -34,33 +38,25 @@ const run = async () => {
       groupMessage(credentials.username);
     }
     else{
-      console.log('Invalid Query');
-  }
-
-  };
+      console.log('Invalid Command');
+    }
+};
   
 run();
 
-
-// Store conversation history
-let conversationHistory;
+// This is a predefine code, I got it from slack only credentials are my own
 // ID of channel you watch to fetch the history for
-let channelId = "C025Q6ELKNZ";
-async function test(){
+async function test(id){
     try {
         // Call the conversations.history method using WebClient
         const result = await client.conversations.history({
-          channel: channelId
+          channel: id
         });
-        //conversationHistory = result.messages;
         let data='';
         result.messages.map(resmap=>{
             data = data + `${resmap.text}
 `;
         });
-        // Print results
-        //console.log(conversationHistory.length + " messages found in " + channelId);
-        //console.log(data);
         console.log(chalk.green(data));
       }
       catch (error) {
@@ -68,22 +64,25 @@ async function test(){
       }
 }
 
+// I've prepared this function by using postman
 function sendtochannel(val){
-  var req = unirest('POST', 'https://hooks.slack.com/services/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')
+  var req = unirest('POST', credential.webhookURL)  //3
   .headers({
     'Content-Type': 'application/json'
   })
   .send(JSON.stringify(val))
   .end(function (res) { 
     if (res.error) throw new Error(res.error); 
-    console.log(res.raw_body);
+    console.log(chalk.blue("Message sent"));
   });
 }
 
+// This function is my own implementation
 function groupMessage(x){
   let dataJSON;
   
   if(x.includes(`channel`) && x.includes(`message`) && x.includes(`username`)){
+
     let channel = x.slice(x.indexOf(`slack channel "`)+15,x.indexOf(`" message "`));
     let text = x.slice(x.indexOf(`" message "`)+11,x.indexOf(`" username "`));
     let username = x.slice(x.indexOf(`" username "`)+12,x.length-1);
@@ -97,6 +96,7 @@ function groupMessage(x){
     sendtochannel(dataJSON);
   }
   else if(x.includes(`channel`) && x.includes(`message`)){
+
     let channel = x.slice(x.indexOf(`slack channel "`)+15,x.indexOf(`" message "`));
     let text = x.slice(x.indexOf(`" message "`)+11,x.length-1);
     dataJSON = {
